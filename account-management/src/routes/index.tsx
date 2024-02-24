f66f7c166cc7e663d33d8fb17bdb72e1bcb0d46c
Rin To Van <rintov@nashtechglobal.com>
2024-02-24 20:57:25 +0700

    Updated routes

---
+++ account-management/src/routes/index.tsx
@@ -0,0 +1,17 @@
+import React, { lazy } from "react";
+import { Route, Routes as RouteList } from "react-router";
+import Admin from "../pages/admin";
+
+interface RoutesProps {
+  children?: React.ReactNode;
+}
+
+const Routes = (props: RoutesProps) => {
+  return (
+    <RouteList>
+      <Route path="/admin" element={<Admin />} />
+    </RouteList>
+  );
+};
+
+export default Routes
