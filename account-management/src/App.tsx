6032f700165893c10874f62a1ca6c796d4c03e68
Rin To Van <rintov@nashtechglobal.com>
2024-03-07 19:30:58 +0700

    Update package

--- account-management/src/App.tsx
+++ account-management/src/App.tsx
@@ -1,8 +1,8 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-import {BrowserRouter} from 'react-router-dom';
-import Routes from './routes'
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './routes';
 function App() {
   return (
     <div className="App">
